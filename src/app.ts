b07d7c6aeb6a301be4e0f0acf1c5ef06c4dadb00
Nurul Amin <nurulaminsw@gmail.com>
2026-07-07 21:42:01 +0600

    Initialize database schema (Prisma + PostgreSQL)
    Added core tables for GearUp (users, gear, rentals, payments, reviews)

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import express, { Application, Request, Response } from "express";
 
-
 const app: Application = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+  res.send("Welcome to Gearup Service!");
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import app from "./app";
+import config from "./config";
+import { prisma } from "./lib/prisma";
 
 async function main() {
+  const PORT = config.port;
   try {
-    app.listen(5000, () => {
-      console.log("Server is running on port 5000");
+    await prisma.$connect();
+    console.log("Prisma Database is connected suscessfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error starting server:", error);
+    await prisma.$disconnect(); // Disconnect from the database
+    process.exit(1); // Exit the process with an error code
+  }
 }
 
 main();
